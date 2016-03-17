bfc213545dcc852183d912043e94071cb62cb250
Dmitry Popov <dmitry@parsec-club.ru>
2016-03-17 12:42:21 +0300

    Initial commit

---
+++ src/WebMarkupMin.AspNet5.FileActionResultError/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNet.Mvc;
+
+namespace WebMarkupMin.AspNet5.FileActionResultError.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("file.xml")]
+        public IActionResult GetFile()
+        {
+            var xml = @"<xml version='1.0'><root><greeting>Hello, World</greeting></root>";
+
+            var xmlBytes = System.Text.Encoding.UTF8.GetBytes(xml);
+
+            return File(xmlBytes, "application/atom+xml", "feed.xml");
+        }
+    }
+}
---
+++ src/WebMarkupMin.AspNet5.FileActionResultError/Startup.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
+using Microsoft.AspNet.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace WebMarkupMin.AspNet5.FileActionResultError
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services
+               .AddWebMarkupMin(o =>
+               {
+                   o.AllowMinificationInDevelopmentEnvironment = true;
+                   o.AllowCompressionInDevelopmentEnvironment = false;
+               })
+               .AddHtmlMinification(o =>
+               {
+                   o.MinificationSettings = new WebMarkupMin.Core.HtmlMinificationSettings(true)
+                   {
+                       WhitespaceMinificationMode = WebMarkupMin.Core.WhitespaceMinificationMode.Safe,
+                   };
+               });
+
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.MinimumLevel = LogLevel.Debug;
+            loggerFactory.AddConsole(LogLevel.Debug);
+
+            app.UseIISPlatformHandler();
+
+            app.UseWebMarkupMin();
+
+            app.UseMvcWithDefaultRoute();
+        }
+
+        // Entry point for the application.
+        public static void Main(string[] args) => WebApplication.Run<Startup>(args);
+    }
+}
